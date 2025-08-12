1ec10c71c10b3b47f74e5698e3a45b812b4e4b19
Lucas Abreu <lucassoliveiraabreu@gmail.com>
2025-08-12 13:33:05 -0300

    feat: Adicionando Dayjs

---
+++ src/libs/dayjs.js
@@ -0,0 +1,4 @@
+import dayjs from 'dayjs';
+import "dayjs/locale/pt-br"; // Importando o locale em português
+
+dayjs.locale('pt-br'); // Definindo o locale para português brasileiro
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//Configuração do Dayjs
+import './libs/dayjs';
+
 //CSS
 import './styles/global.css';
 import './styles/form.css';

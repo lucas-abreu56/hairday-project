21bc672b9e3e79c488682021e466d41d79f00e03
Lucas Abreu <lucassoliveiraabreu@gmail.com>
2025-08-14 21:42:00 -0300

    feat: Renderiza os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,17 @@
-import { hoursLoad } from "../form/hours.load.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { hoursLoad } from "../form/hours-load.js";
+import { schedulesShow } from "../schedules/show.js";
 
 const selectedDate = document.getElementById('date');
-export function scheduleDay(){
+export async function scheduleDay(){
     //Obtém a data do input
     const date = selectedDate.value;
+
+    //Busca na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({date});
+    
+    //Exibe os agendamentos
+    schedulesShow({ dailySchedules })
 
     // Renderiza as horas disponíveis
     hoursLoad({ date });
